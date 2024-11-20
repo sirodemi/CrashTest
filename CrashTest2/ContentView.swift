23c4f263c14d06b4959ec9c754ea3369b0970d46
衣斐司郎 <shiro@sunyou.co.jp>
2024-11-20 19:06:45 +0900

    Initial Commit

---
+++ CrashTest2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CrashTest2
+//
+//  Created by shiro ibi on 2024/11/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CrashTest2/CrashTest2App.swift
@@ -0,0 +1,17 @@
+//
+//  CrashTest2App.swift
+//  CrashTest2
+//
+//  Created by shiro ibi on 2024/11/20.
+//
+
+import SwiftUI
+
+@main
+struct CrashTest2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
